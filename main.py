8ead474fe17cfeda9a01eaa612e903c824b52b11
deeksha <hegde_deeksha@lilly.com>
2022-09-15 12:46:01 +0530

    Main file for calculating total salary

---
+++ main.py
@@ -0,0 +1,8 @@
+import cal
+fname=input("Enter Your First Name: ")
+lname=input("Enter Your Last Name: ")
+sal2=cal.take_salary()
+hra2=cal.hra()
+da2=cal.da()
+bonus2=cal.bonus()
+print("Total salary: ",sal2+hra2+da2+bonus2)
