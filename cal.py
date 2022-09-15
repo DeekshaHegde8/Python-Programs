7c3a279453a897a2f04c35bd36fd6eb7192bba35
deeksha <hegde_deeksha@lilly.com>
2022-09-15 12:47:21 +0530

    File for  functions salary,HRA,DA,Bonus

---
+++ cal.py
@@ -0,0 +1,20 @@
+sal1=0
+def take_salary():
+    global sal1
+    print("Enter your salary: ")
+    sal1=int(input())
+    return sal1
+    
+
+def hra():
+    global sal1
+    hra1=((10/100)*sal1)
+    return hra1
+def da():
+    global sal1
+    da1=((30/100)*sal1)
+    return da1
+def bonus():
+    global sal1
+    bonus1=((10/100)*sal1)
+    return bonus1
